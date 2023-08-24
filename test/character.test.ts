8067d454c776022c8783fc3e5c4900f7c38ea80c
HenriqueContini <henrique.coutine@gmail.com>
2023-08-23 21:37:51 -0300

    Get character by id tests

--- test/character.test.ts
+++ test/character.test.ts
@@ -3,7 +3,7 @@ import app from "../src/app";
 
 const ENDPOINT = "/character";
 
-describe("Character tests", () => {
+describe("All Characters tests", () => {
   it("Get with sucess all characters", async () => {
     const { statusCode, body } = await request(app).get(ENDPOINT);
 
@@ -45,6 +45,35 @@ describe("Character tests", () => {
       .query("page=test");
 
     expect(statusCode).toBe(500);
+    expect(body.error).toBe(true);
+    expect(body.errorMessage).toBeDefined();
+  });
+});
+
+describe("One Character test", () => {
+  it("Get with sucess character with ID: 1", async () => {
+    const id = 1;
+    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
+
+    expect(statusCode).toBe(200);
+    expect(body.error).toBe(false);
+    expect(body.data).not.toBeNull();
+    expect(body.data.id).toBe(id);
+  });
+
+  it("Get Bad Request when character ID type is a string", async () => {
+    const id = "test";
+    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
+
+    expect(statusCode).toBe(400);
+    expect(body.error).toBe(true);
+  });
+
+  it("Get Not Found when character ID doesn't exists", async () => {
+    const id = 2000;
+    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
+
+    expect(statusCode).toBe(404);
     expect(body.error).toBe(true);
     expect(body.errorMessage).toBeDefined();
   });
