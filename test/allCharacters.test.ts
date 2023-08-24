f6b87dc9889cdbf2828a90d4c12edcd9f740bcfd
HenriqueContini <henrique.coutine@gmail.com>
2023-08-23 21:45:13 -0300

    Refactor: Splitting tests between files

--- test/allCharacters.test.ts
+++ test/allCharacters.test.ts
@@ -39,41 +39,12 @@ describe("All Characters tests", () => {
     expect(body.data).not.toBeNull();
   });
 
-  it("Get Internal Server Error when page type is a string", async () => {
+  it("Get Bad Request when page type is a string", async () => {
     const { statusCode, body } = await request(app)
       .get(`${ENDPOINT}`)
       .query("page=test");
 
-    expect(statusCode).toBe(500);
-    expect(body.error).toBe(true);
-    expect(body.errorMessage).toBeDefined();
-  });
-});
-
-describe("One Character test", () => {
-  it("Get with sucess character with ID: 1", async () => {
-    const id = 1;
-    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
-
-    expect(statusCode).toBe(200);
-    expect(body.error).toBe(false);
-    expect(body.data).not.toBeNull();
-    expect(body.data.id).toBe(id);
-  });
-
-  it("Get Bad Request when character ID type is a string", async () => {
-    const id = "test";
-    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
-
     expect(statusCode).toBe(400);
-    expect(body.error).toBe(true);
-  });
-
-  it("Get Not Found when character ID doesn't exists", async () => {
-    const id = 2000;
-    const { statusCode, body } = await request(app).get(`${ENDPOINT}/${id}`);
-
-    expect(statusCode).toBe(404);
     expect(body.error).toBe(true);
     expect(body.errorMessage).toBeDefined();
   });
